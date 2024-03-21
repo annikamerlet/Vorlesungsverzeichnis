eff573177afed41d74d629a03412ff4471e69c9b
Annika <annika.merlet@haw-hamburg.de>
2024-03-21 11:21:47 +0100

    Klasse: Vorlesung

--- src/main/java/com/example/demo/Vorlesung.java
+++ src/main/java/com/example/demo/Vorlesung.java
@@ -1,14 +1,37 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Vorlesung {
-    private String Bezeichnung;
-    private String Wochentag;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String bezeichnung;
+    private String wochentag;
+
+    public Vorlesung() {
+        // required by hibernate
+    }
+
+    public Vorlesung(String bezeichnung, String wochentag) {
+        this.bezeichnung = bezeichnung;
+        this.wochentag = wochentag;
+    }
+
+    public Long getId() {
+        return id;
+    }
 
     public String getBezeichnung() {
-        return Bezeichnung;
+        return bezeichnung;
     }
 
     public String getWochentag() {
-        return Wochentag;
+        return wochentag;
     }
+
 }
--- src/main/java/com/example/demo/VorlesungsverzeichnisRepository.java
+++ src/main/java/com/example/demo/VorlesungsverzeichnisRepository.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface VorlesungsverzeichnisRepository {
+import java.util.List;
 
+@Repository
+public interface VorlesungsverzeichnisRepository extends CrudRepository<Vorlesung, Long> {
+    @Override
+    List<Vorlesung> findAll();
 }
