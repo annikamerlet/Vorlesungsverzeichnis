9e285e2157bc8b473d547d43f9ca474180b4e13d
Annika <annika.merlet@haw-hamburg.de>
2024-03-22 14:04:24 +0100

    Buttons zum Auswählen html 2

--- src/main/java/com/example/demo/Stundenplan.java
+++ src/main/java/com/example/demo/Stundenplan.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
 public class Stundenplan {
-
     //HashTable : Index entspricht dem Wochentag; über den Index wird auf die Liste der (für den jeweiligen Tag ausgewählten) Vorlesungen zugegriffen
 }
--- src/main/java/com/example/demo/VorlesungsverzeichnisRepository.java
+++ src/main/java/com/example/demo/VorlesungsverzeichnisRepository.java
@@ -1,12 +1,8 @@
 package com.example.demo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface VorlesungsverzeichnisRepository extends CrudRepository<Vorlesung, Long> {
-    @Override
-    List<Vorlesung> findAll();
+public interface VorlesungsverzeichnisRepository extends JpaRepository<Vorlesung, Long> {
 }
